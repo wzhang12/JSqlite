9394e036c7295e59cfd0821ace93844173425381
clark <wdsgyj@gmail.com>
2014-08-28 20:03:49 +0800

    [初始化]

--- src/java/test/com/baidu/test/DatabaseTest.java
+++ src/java/test/com/baidu/test/DatabaseTest.java
@@ -1,9 +1,6 @@
 package com.baidu.test;
 
-import com.baidu.javalite.DBConnection;
-import com.baidu.javalite.Database;
-import com.baidu.javalite.Flags;
-import com.baidu.javalite.SqliteException;
+import com.baidu.javalite.*;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -33,21 +30,65 @@ public class DatabaseTest {
 
         // 如果数据库文件不存在，那么使用 SQLITE_OPEN_READONLY 打开数据库会抛出异常
         try {
-            database.open(Flags.SQLITE_OPEN_READONLY);
-            Assert.assertTrue(false);
+            conn = null;
+            conn = database.open(Flags.SQLITE_OPEN_READONLY);
+            Assert.assertTrue("如果数据库文件不存在，那么使用 SQLITE_OPEN_READONLY 打开数据库会抛出异常!", false);
         } catch (SqliteException e) {
-            Assert.assertTrue("如果数据库文件不存在，那么使用 SQLITE_OPEN_READONLY 打开数据库会抛出异常!", true);
+            Assert.assertTrue(true);
+        } finally {
+            DBHelper.closeQuietly(conn);
         }
 
         // 如果数据库不存在，那么使用 SQLITE_OPEN_READWRITE 打开数据库会抛出异常
         try {
-            database.open(Flags.SQLITE_OPEN_READWRITE);
-            Assert.assertTrue(false);
+            conn = null;
+            conn = database.open(Flags.SQLITE_OPEN_READWRITE);
+            Assert.assertTrue("如果数据库不存在，那么使用 SQLITE_OPEN_READWRITE 打开数据库会抛出异常!", false);
         } catch (SqliteException e) {
-            Assert.assertTrue("如果数据库不存在，那么使用 SQLITE_OPEN_READWRITE 打开数据库会抛出异常!", true);
+            Assert.assertTrue(true);
+        } finally {
+            DBHelper.closeQuietly(conn);
         }
-
-
     }
 
+    @Test
+    public void testIsCompiledThreadsafe() {
+        try {
+            Database.isCompiledThreadsafe();
+            Assert.assertTrue(true);
+        } catch (Exception e) {
+            Assert.assertTrue("isCompiledThreadsafe() 方法调用失败！", false);
+        }
+    }
+
+    @Test
+    public void testInitializeLibrary() {
+        try {
+            Database.initializeLibrary();
+            Assert.assertTrue(true);
+        } catch (SqliteException e) {
+            Assert.assertTrue("initializeLibrary() 方法调用失败！", false);
+        }
+    }
+
+    @Test
+    public void testShutdownLibrary() {
+        try {
+            Database.shutdownLibrary();
+            Assert.assertTrue(true);
+        } catch (SqliteException e) {
+            Assert.assertTrue("shutdownLibrary() 方法调用失败！", false);
+        }
+    }
+
+    @Test
+    public void testVersionApi() {
+        try {
+            Assert.assertTrue("getLibrarySourceId() 方法调用失败！", Database.getLibrarySourceId() != null);
+            Assert.assertTrue("getLibraryVersion() 方法调用失败！", Database.getLibraryVersion() != null);
+            Assert.assertTrue("getLibraryVersionNumber() 方法调用失败！", Database.getLibraryVersionNumber() != 0);
+        } catch (Exception e) {
+            Assert.assertTrue("getLibraryXXX() 方法调用失败！", false);
+        }
+    }
 }

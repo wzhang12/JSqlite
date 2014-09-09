e25f62665556d8f4027ee8cbb44af302018f994b
clark <wdsgyj@126.com>
2014-09-09 23:25:05 +0800

    [init]

--- src/java/main/com/baidu/javalite/AggregateFunction.java
+++ src/java/main/com/baidu/javalite/AggregateFunction.java
@@ -4,7 +4,7 @@ package com.baidu.javalite;
  * Created by clark on 14-9-4.
  */
 public interface AggregateFunction extends ScalarFunction {
-    void xStep(Context context, Value[] values);
+    void xStep(Context context, Value[] values) throws SqliteException;
 
-    void xFinal(Context context);
+    void xFinal(Context context) throws SqliteException;
 }
--- src/java/main/com/baidu/javalite/ScalarFunction.java
+++ src/java/main/com/baidu/javalite/ScalarFunction.java
@@ -4,5 +4,5 @@ package com.baidu.javalite;
  * Created by clark on 14-9-4.
  */
 public interface ScalarFunction {
-    void xFunc(Context context, Value[] values);
+    void xFunc(Context context, Value[] values) throws SqliteException;
 }

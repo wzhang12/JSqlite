1bdfad50056ed2a9b079c280f572aafe7419819e
clark <wdsgyj@126.com>
2014-09-09 15:49:02 +0800

    [init]

--- src/java/main/com/baidu/javalite/Value.java
+++ src/java/main/com/baidu/javalite/Value.java
@@ -10,6 +10,34 @@ public class Value {
         this.handle = handle;
     }
 
+    public int getType() throws SqliteException {
+        return sqlite3_value_type(handle);
+    }
+
+    public int getNumericType() throws SqliteException {
+        return sqlite3_value_numeric_type(handle);
+    }
+
+    public byte[] getBlob() throws SqliteException {
+        return sqlite3_value_blob(handle);
+    }
+
+    public double getDouble() throws SqliteException {
+        return sqlite3_value_double(handle);
+    }
+
+    public int getInt() throws SqliteException {
+        return sqlite3_value_int(handle);
+    }
+
+    public long getInt64() throws SqliteException {
+        return sqlite3_value_int64(handle);
+    }
+
+    public String getText() throws SqliteException {
+        return sqlite3_value_text(handle);
+    }
+
     private static native byte[] sqlite3_value_blob(long handle) throws SqliteException;
 
     private static native double sqlite3_value_double(long handle) throws SqliteException;

f23fab5613c3922aa4589780cc8ede53bb2374ab
clark <wdsgyj@126.com>
2014-09-15 18:03:17 +0800

    init

--- src/java/main/com/baidu/javalite/NativeRuntime.java
+++ src/java/main/com/baidu/javalite/NativeRuntime.java
@@ -1,19 +1,38 @@
 package com.baidu.javalite;
 
+import java.io.File;
+import java.net.URISyntaxException;
+
 /**
  * Created by clark on 14-9-15.
  */
 public class NativeRuntime {
 
+    public static final String J_SQLITE_LIB_NAME = "JSqlite";
     private boolean inited;  // init with false
 
     private NativeRuntime() {}
 
     public void load() {
         if (!inited) {
-            System.loadLibrary("JSqlite");
+            if (!findNativeLibPath()) {
+                System.loadLibrary(J_SQLITE_LIB_NAME);
+            }
             inited = true;
         }
+    }
+
+    private boolean findNativeLibPath() {
+        try {
+            File jar = new File(NativeRuntime.class.getProtectionDomain().getCodeSource().getLocation().toURI());
+            String parent = jar.getParent();
+            String libfileName = System.mapLibraryName(J_SQLITE_LIB_NAME);
+            System.load(new File(parent, libfileName).getAbsolutePath());
+            return true;
+        } catch (URISyntaxException e) {
+        }
+
+        return false;
     }
 
     private static class Holder {

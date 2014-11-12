c3977ae3c7648c38fb1e2949a08daf8a8e8b82f1
clark <wdsgyj@126.com>
2014-11-12 16:12:13 +0800

    添加 scala 代码

--- src/scala/com/baidu/scalalite/Connection.scala
+++ src/scala/com/baidu/scalalite/Connection.scala
@@ -7,10 +7,12 @@ import com.baidu.javalite.{Authorizer, ProfileListener, TraceListener, Collation
  */
 class Connection protected[scalalite](private[this] val conn: DBConnection) {
   @inline def exec(sql: String)(cb: (Int, Array[String], Array[String]) => Int) =
-    conn.exec(sql, new SqlExecCallback {
-      override def callback(ncols: Int, values: Array[String], headers: Array[String]): Int =
-        cb(ncols, values, headers)
-    })
+    conn.exec(sql,
+      if (cb == null) null
+      else new SqlExecCallback {
+        override def callback(ncols: Int, values: Array[String], headers: Array[String]): Int =
+          cb(ncols, values, headers)
+      })
 
   @inline def exec(sql: String) = conn.exec(sql)
 
@@ -23,34 +25,44 @@ class Connection protected[scalalite](private[this] val conn: DBConnection) {
   @inline def getTable(sql: String) = conn.getTable(sql)
 
   @inline def prepare(sql: String)(op: (PrepareStmt) => Unit): Unit = {
-    val stmt = conn.prepare(sql)
-    try op(stmt) finally DBHelper.closeQuietly(stmt)
+    if (op != null) {
+      val stmt = conn.prepare(sql)
+      try op(stmt) finally DBHelper.closeQuietly(stmt)
+    }
   }
 
   @inline def newTransaction = conn.newTransaction()
 
   @inline def setBusyHandler(handler: (Int) => Int) =
-    conn.setBusyHandler(new BusyHandler {
-      override def handle(times: Int): Int = handler(times)
-    })
+    conn.setBusyHandler(
+      if (handler == null) null
+      else new BusyHandler {
+        override def handle(times: Int): Int = handler(times)
+      })
 
   @inline def setBusyTimeout(ms: Int) = conn.setBusyTimeout(ms)
 
   @inline def setCommitHook(hook: (Connection) => Int) =
-    conn.setCommitHook(new CommitHook {
-      override def callback(dbConn: DBConnection): Int = hook(new Connection(dbConn))
-    })
+    conn.setCommitHook(
+      if (hook == null) null
+      else new CommitHook {
+        override def callback(dbConn: DBConnection): Int = hook(new Connection(dbConn))
+      })
 
   @inline def setRollbackHook(hook: (Connection) => Unit) =
-    conn.setRollbackHook(new RollbackHook {
-      override def callback(conn: DBConnection): Unit = hook(new Connection(conn))
-    })
+    conn.setRollbackHook(
+      if (hook == null) null
+      else new RollbackHook {
+        override def callback(conn: DBConnection): Unit = hook(new Connection(conn))
+      })
 
   @inline def setUpdateHook(hk: (Connection, Int, String, String, Long) => Unit) =
-    conn.setUpdateHook(new UpdateHook {
-      override def hook(conn: DBConnection, action: Int, dbname: String, tbname: String, rowid: Long): Unit =
-        hk(new Connection(conn), action, dbname, tbname, rowid)
-    })
+    conn.setUpdateHook(
+      if (hk == null) null
+      else new UpdateHook {
+        override def hook(conn: DBConnection, action: Int, dbname: String, tbname: String, rowid: Long): Unit =
+          hk(new Connection(conn), action, dbname, tbname, rowid)
+      })
 
   @inline def getReadWriteBlob(dbName: String, tbName: String, colName: String, rowId: Long) =
     conn.getReadWriteBlob(dbName, tbName, colName, rowId)
@@ -59,9 +71,11 @@ class Connection protected[scalalite](private[this] val conn: DBConnection) {
     conn.getReadOnlyBlob(dbName, tbName, colName, rowId)
 
   @inline def createFunction(funcName: String, nArgs: Int, app: Any)(func: (Context, Array[Value]) => Unit) =
-    conn.createFunction(funcName, nArgs, app, new ScalarFunction {
-      override def xFunc(context: Context, values: Array[Value]): Unit = if (func != null) func(context, values)
-    })
+    conn.createFunction(funcName, nArgs, app,
+      if (func == null) null
+      else new ScalarFunction {
+        override def xFunc(context: Context, values: Array[Value]): Unit = func(context, values)
+      })
 
   @inline def createAggregate(funcName: String, nArgs: Int, app: Any)
                              (func: (Context, Array[Value]) => Unit)
@@ -92,22 +106,24 @@ class Connection protected[scalalite](private[this] val conn: DBConnection) {
   @inline def nextStmt(old: PrepareStmt) = conn.nextStmt(old)
 
   @inline def setOnTraceListener(lsn: (Connection, String) => Unit) =
-    if (lsn == null) conn.setOnTraceListener(null)
-    else
-      conn.setOnTraceListener(new TraceListener {
+    conn.setOnTraceListener(
+      if (lsn == null) null
+      else new TraceListener {
         override def trace(conn: DBConnection, msg: String): Unit = lsn(new Connection(conn), msg)
       })
 
   @inline def setOnProfileListener(pf: (Connection, String, Long) => Unit) =
-    if (pf == null) conn.setOnProfileListener(null)
-    else conn.setOnProfileListener(new ProfileListener {
-      override def profile(conn: DBConnection, msg: String, time: Long): Unit = pf(new Connection(conn), msg, time)
-    })
+    conn.setOnProfileListener(
+      if (pf == null) null
+      else new ProfileListener {
+        override def profile(conn: DBConnection, msg: String, time: Long): Unit = pf(new Connection(conn), msg, time)
+      })
 
   @inline def setAuthorizer(auth: (Connection, Int, String, String, String, String) => Int) =
-    if (auth == null) conn.setAuthorizer(null)
-    else conn.setAuthorizer(new Authorizer {
-      override def xAuth(conn: DBConnection, action: Int, s1: String, s2: String, s3: String, s4: String): Int =
-        auth(new Connection(conn), action, s1, s2, s3, s4)
-    })
+    conn.setAuthorizer(
+      if (auth == null) null
+      else new Authorizer {
+        override def xAuth(conn: DBConnection, action: Int, s1: String, s2: String, s3: String, s4: String): Int =
+          auth(new Connection(conn), action, s1, s2, s3, s4)
+      })
 }
